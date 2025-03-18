38c4e53d97854e1d3c9ccfdf5fef151a4d35c8d3
Marko Poutiainen <Marko.Poutiainen@bittium.com>
2025-03-18 13:21:39 +0200

    Added better options for tabs.

--- plugin/options.lua
+++ plugin/options.lua
@@ -9,6 +9,12 @@ opt.inccommand = "split"
 opt.smartcase = true
 opt.ignorecase = true
 
+-- Tabs
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
 -- Show line numbers
 opt.number = true
 

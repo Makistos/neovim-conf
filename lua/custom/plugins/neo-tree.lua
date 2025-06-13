fee0c94119a29a1f3036dae77060d87cc7f4fc62
Marko Poutiainen <Marko.Poutiainen@bittium.com>
2025-06-13 13:43:06 +0300

    Added keys for switching between files and buffers in Neotree

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -7,5 +7,19 @@ if not vim.g.vscode then
 			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 			"MunifTanjim/nui.nvim",
 		},
+		config = function()
+			vim.keymap.set(
+				"n",
+				"<leader>nb",
+				"<Cmd>Neotree buffers<CR>",
+				{ desc = "Toggle Neotree buffers", silent = true }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>nf",
+				"<Cmd>Neotree filesystem<CR>",
+				{ desc = "Toggle Neotree filesystem", silent = true }
+			)
+		end,
 	}
 end

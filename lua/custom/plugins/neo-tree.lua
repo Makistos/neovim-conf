76b48ffb57ac3eb49db0da394a9410a575977431
Marko Poutiainen <Marko.Poutiainen@bittium.com>
2025-06-23 08:43:30 +0300

    Fixed startup issue with neo-tree

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -2,6 +2,7 @@ if not vim.g.vscode then
 	return {
 		"nvim-neo-tree/neo-tree.nvim",
 		version = "v3.x",
+		lazy = false,
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended

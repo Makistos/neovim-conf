d81e50be8fa3df9906d89a381a3aa8345b4419a6
Marko Poutiainen <marko.poutiainen@bittium.com>
2026-01-23 16:19:53 +0200

    Added linting and formatting support for more languages to conform

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -2,34 +2,25 @@
 if not vim.g.vscode then
 	return {
 		"stevearc/conform.nvim",
-		-- Load immediately so RuffProject command exists
 		lazy = false,
 		event = { "BufWritePre" },
 		cmd = { "ConformInfo" },
 		keys = {
 			{
-				"<leader>co",
+				"<leader>f",
 				function()
 					require("conform").format({ async = true, lsp_format = "never" })
 				end,
 				mode = "",
-				desc = "F[o]rmat buffer",
-			},
-			{
-				"<leader>cl",
-				"<cmd>RuffProject<cr>",
-				mode = "n",
-				desc = "Lint Python project with Ruff",
+				desc = "[F]ormat buffer",
 			},
 		},
-
 		config = function()
 			require("conform").setup({
 				notify_on_error = false,
 				format_on_save = function(bufnr)
 					local ft = vim.bo[bufnr].filetype
 					local lsp_timeout, lsp_format_opt
-
 					if ft == "python" or ft == "lua" then
 						lsp_timeout = 1000
 						lsp_format_opt = "never"
@@ -37,65 +28,169 @@ if not vim.g.vscode then
 						lsp_timeout = 500
 						lsp_format_opt = "fallback"
 					end
-
 					return {
 						timeout_ms = lsp_timeout,
 						lsp_format = lsp_format_opt,
 					}
 				end,
 				formatters_by_ft = {
-					python = { "ruff_format" },
+					python = { "black" },
 					cpp = { "clang-format" },
 					c = { "clang-format" },
 					lua = { "stylua" },
-					-- python = { "isort", "black" }, -- optional sequential formatters
+					typescript = { "prettier" },
+					typescriptreact = { "prettier" },
+					javascript = { "prettier" },
+					javascriptreact = { "prettier" },
+					go = { "gofmt" },
 				},
-
 			})
-			-- Register RuffProject command
-			vim.api.nvim_create_user_command("RuffProject", function()
-				local cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] or vim.fn.getcwd()
-				local cmd = string.format("ruff check %s", cwd)
-				local output = vim.fn.systemlist(cmd)
-				vim.fn.setqflist({}, " ", {
-					title = "Ruff Lint",
-					lines = output -- vim.split(vim.fn.system(cmd), "\n"),
-				})
-				if #output > 0 then
-					vim.cmd("copen")
-				end
-			end, { desc = "Run Ruff linter on the entire project and show results" })
-			-- Ruff fix command for current line or selected quickfix entry
-		vim.keymap.set("n", "cc", "<cmd>RuffProject<cr>", { desc = "Run Ruff check on project", silent = true })
-		end,
 
-		-- opts = {
-		-- 	notify_on_error = false,
-		-- 	format_on_save = function(bufnr)
-		-- 		local ft = vim.bo[bufnr].filetype
-		-- 		local lsp_timeout, lsp_format_opt
-		--
-		-- 		if ft == "python" or ft == "lua" then
-		-- 			lsp_timeout = 1000
-		-- 			lsp_format_opt = "never"
-		-- 		else
-		-- 			lsp_timeout = 500
-		-- 			lsp_format_opt = "fallback"
-		-- 		end
-		--
-		-- 		return {
-		-- 			timeout_ms = lsp_timeout,
-		-- 			lsp_format = lsp_format_opt,
-		-- 		}
-		-- 	end,
-		-- 	formatters_by_ft = {
-		-- 		python = { "ruff_format" },
-		-- 		cpp = { "clang-format" },
-		-- 		c = { "clang-format" },
-		-- 		lua = { "stylua" },
-		-- 		-- python = { "isort", "black" }, -- optional sequential formatters
-		-- 	},
-		-- },
+			-- Linter commands by filetype (can have multiple per filetype)
+			local linters = {
+				python = {
+					{
+						cmd = function()
+							local cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] or vim.fn.getcwd()
+							return string.format("ruff check %s", cwd)
+						end,
+						title = "Ruff",
+					},
+				},
+				c = {
+					{
+						cmd = function()
+							local file = vim.fn.expand("%:p")
+							return string.format("cppcheck --enable=all --suppress=missingIncludeSystem %s 2>&1", file)
+						end,
+						title = "Cppcheck",
+					},
+					{
+						cmd = function()
+							local file = vim.fn.expand("%:p")
+							local cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] or vim.fn.getcwd()
+							return string.format("clang-tidy %s -p %s 2>&1", file, cwd)
+						end,
+						title = "Clang-Tidy",
+					},
+				},
+				cpp = {
+					{
+						cmd = function()
+							local file = vim.fn.expand("%:p")
+							return string.format("cppcheck --enable=all --suppress=missingIncludeSystem %s 2>&1", file)
+						end,
+						title = "Cppcheck",
+					},
+					{
+						cmd = function()
+							local file = vim.fn.expand("%:p")
+							local cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] or vim.fn.getcwd()
+							return string.format("clang-tidy %s -p %s 2>&1", file, cwd)
+						end,
+						title = "Clang-Tidy",
+					},
+				},
+				lua = {
+					{
+						cmd = function()
+							local file = vim.fn.expand("%:p")
+							return string.format("luacheck %s", file)
+						end,
+						title = "Luacheck",
+					},
+				},
+				typescript = {
+					{
+						cmd = function()
+							local cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] or vim.fn.getcwd()
+							return string.format("npx eslint %s --format unix 2>&1", cwd)
+						end,
+						title = "ESLint",
+					},
+				},
+				typescriptreact = {
+					{
+						cmd = function()
+							local cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] or vim.fn.getcwd()
+							return string.format("npx eslint %s --format unix 2>&1", cwd)
+						end,
+						title = "ESLint",
+					},
+				},
+				javascript = {
+					{
+						cmd = function()
+							local cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] or vim.fn.getcwd()
+							return string.format("npx eslint %s --format unix 2>&1", cwd)
+						end,
+						title = "ESLint",
+					},
+				},
+				javascriptreact = {
+					{
+						cmd = function()
+							local cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] or vim.fn.getcwd()
+							return string.format("npx eslint %s --format unix 2>&1", cwd)
+						end,
+						title = "ESLint",
+					},
+				},
+				go = {
+					{
+						cmd = function()
+							local cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] or vim.fn.getcwd()
+							return string.format("staticcheck %s/... 2>&1", cwd)
+						end,
+						title = "Staticcheck",
+					},
+					{
+						cmd = function()
+							local cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1] or vim.fn.getcwd()
+							return string.format("go vet %s/... 2>&1", cwd)
+						end,
+						title = "Go Vet",
+					},
+				},
+			}
+
+			-- Run all linters for current filetype and populate quickfix
+			vim.api.nvim_create_user_command("Lint", function()
+				local ft = vim.bo.filetype
+				local ft_linters = linters[ft]
+
+				if not ft_linters then
+					vim.notify("No linter configured for filetype: " .. ft, vim.log.levels.WARN)
+					return
+				end
+
+				local all_output = {}
+				local titles = {}
+
+				for _, linter in ipairs(ft_linters) do
+					table.insert(titles, linter.title)
+					local cmd = linter.cmd()
+					local output = vim.fn.systemlist(cmd)
+					for _, line in ipairs(output) do
+						if line ~= "" then
+							table.insert(all_output, line)
+						end
+					end
+				end
+
+				vim.fn.setqflist({}, " ", {
+					title = "Lint [" .. table.concat(titles, ", ") .. "]",
+					lines = all_output,
+				})
+
+				if #all_output > 0 then
+					vim.cmd("copen")
+				else
+					vim.notify("No issues found", vim.log.levels.INFO)
+				end
+			end, { desc = "Run all linters for current filetype" })
+
+			vim.keymap.set("n", "cc", "<cmd>Lint<cr>", { desc = "Run linters for filetype", silent = true })
+		end,
 	}
 end
-
